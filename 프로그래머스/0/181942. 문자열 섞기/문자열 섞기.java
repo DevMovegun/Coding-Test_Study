0fbf1815f11ddd0ef1f5c380e3fa995741b8b909
Donggeon Lee <developer_movegun@naver.com>
2024-02-02 15:19:14 +0900

    [level 0] Title: 문자열 섞기, Time: 1.25 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -2,26 +2,10 @@ class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
         
-        // for문의 끝을 파악하기 위한 용도
-        int len = str1.length() + str2.length();
-        char[] chArr = new char[len];
-        
-        // 현재 위치
-        int s1 = 0;
-        int s2 = 0;
-        
-        for(int i = 0; i < len; i++) {
-            if(i % 2 == 0) {
-                chArr[i] = str1.charAt(s1);
-                s1++;
-            }
-            else {
-                chArr[i] = str2.charAt(s2);
-                s2++;
-            }
+        for(int i = 0; i < str1.length(); i++) {
+            answer += String.valueOf(str1.charAt(i));
+            answer += String.valueOf(str2.charAt(i));
         }
-        
-        answer = String.valueOf(chArr);
         
         return answer;
     }
