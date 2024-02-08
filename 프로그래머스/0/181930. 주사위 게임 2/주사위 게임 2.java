9be78b7f2bde3bb89ea45cd03623a43f886efb39
Donggeon Lee <developer_movegun@naver.com>
2024-02-09 01:39:19 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.07 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        
+        int sum1 = a + b + c;
+        int sum2 = (int)Math.pow(a,2) + (int)Math.pow(b,2) + (int)Math.pow(c,2);
+        int sum3 = (int)Math.pow(a,3) + (int)Math.pow(b,3) + (int)Math.pow(c,3);
+        
+        if(a == b && b == c)
+            answer = sum1 * sum2 * sum3;
+        else if(a != b && b != c && c != a)
+            answer = sum1;
+        else
+            answer = sum1 * sum2;
+        
+        return answer;
+    }
+}
